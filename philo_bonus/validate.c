a4b649fb253c1ad1708e2d75b6638d925454489d
Caroline Ogata <cogata@c1r8p2.42sp.org.br>
2024-06-20 18:47:18 -0300

    ok

--- philo_bonus/validate.c
+++ philo_bonus/validate.c
@@ -6,11 +6,40 @@
 /*   By: cogata <cogata@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 11:02:31 by cogata            #+#    #+#             */
-/*   Updated: 2024/06/19 15:05:36 by cogata           ###   ########.fr       */
+/*   Updated: 2024/06/20 18:25:21 by cogata           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers_bonus.h"
+
+void	check_sign(char *argv[], int i, int *j, bool *sign)
+{
+	if (argv[i][*j] == '+' || argv[i][*j] == '-')
+	{
+		*sign = true;
+		if (argv[i][*j] == '-')
+			error_exit("Invalid negative number.");
+		else
+			*j = *j + 1;
+	}
+}
+
+void	check_zero(int argc, char *argv[])
+{
+	if (ft_long_atoi(argv[1]) == 0)
+		error_exit("Number of philosophers must be bigger than 0");
+	if (argc == 6 && ft_long_atoi(argv[5]) == 0)
+		exit(0);
+}
+
+void	check_size(char *str)
+{
+	int	len;
+
+	len = ft_strlen(str);
+	if (len > 10 || (len == 10 && ft_strcmp(str, "2147483647") > 0))
+		error_exit("Argument is too big");
+}
 
 void	validate_arguments(int argc, char *argv[])
 {
@@ -36,33 +65,4 @@ void	validate_arguments(int argc, char *argv[])
 		i++;
 	}
 	check_zero(argc, argv);
-}
-
-void	check_sign(char *argv[], int i, int *j, bool *sign)
-{
-	if (argv[i][*j] == '+' || argv[i][*j] == '-')
-	{
-		*sign = true;
-		if (argv[i][*j] == '-')
-			error_exit("Invalid negative number.");
-		else
-			*j = *j + 1;
-	}
-}
-
-void	check_zero(int argc, char *argv[])
-{
-	if (ft_long_atoi(argv[1]) == 0)
-		error_exit("Number of philosophers must be bigger than 0");
-	if (argc == 6 && ft_long_atoi(argv[5]) == 0)
-		error_exit("Number of meals must be bigger than 0");
-}
-
-void	check_size(char *str)
-{
-	int	len;
-
-	len = ft_strlen(str);
-	if (len > 20 || (len == 20 && ft_strcmp(str, "18446744073709551615") > 0))
-		error_exit("Argument is too big");
 }
